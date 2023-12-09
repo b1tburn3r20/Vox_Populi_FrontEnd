c372978ab220066fec17f3d60ff37c779230efbe
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-12-08 19:07:06 -0600

    Auth0 and unbiased Facts

---
+++ app/bills/page.jsx
@@ -0,0 +1,11 @@
+import UnbiasedFacts from '../../components/UnbiasedFacts';
+
+export default function Bills() {
+  return (
+    <>
+      <p>
+        These Are: <UnbiasedFacts />
+      </p>
+    </>
+  );
+}
